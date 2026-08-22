12972d8dc6b6877ae19b8f58fa55362efa2846c3
ittidesu <wakame-wakame-@outlook.jp>
2026-08-22 19:57:41 +0900

    Create AdminNotificationScreen.js

---
+++ src/components/AdminNotificationScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminNotificationScreen() {
+  return (
+    <div>
+      <h1>Admin Notification</h1>
+    </div>
+  );
+}
+
+export default AdminNotificationScreen;
